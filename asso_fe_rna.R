fa85ab8bde59cbaafe6c39d95af26cf64b67c985
Xiang Zhang <sc.xiang.zhang@gmail.com>
2020-05-15 14:25:29 +0200

    Leave 2 month group from NB regression analysis

--- asso_fe_rna.R
+++ asso_fe_rna.R
@@ -12,16 +12,23 @@ library(multidplyr)
 library(broom)
 library(stringr)
 library(forcats)
+library(ggrepel)
 
+###########################################
+# 1. Import data
+###########################################
 
+# negative binomial regression model
 run_edgeR <- function(dat.x, dat.y) {
   fer <- dat.x$fer
   
   zFER <- ( fer - mean(fer) ) / sd(fer)
   
   Batch <- factor(dat.x$Reagent)
-  zAge <- (dat.x$age_day - mean(dat.x$age_day)) / sd(dat.x$age_day)
-  mod1 <- model.matrix(~Batch + zAge + zFER)
+  Group <- factor(dat.x$group, levels = c("1 month", "3 month", "6 month"))
+  #zAge <- (dat.x$age_day - mean(dat.x$age_day)) / sd(dat.x$age_day)
+  #mod1 <- model.matrix(~Batch + zAge + zFER)
+  mod1 <- model.matrix(~Batch + zFER)
   my_dispersions <- estimateDisp(dat.y, design = mod1)
   my_fits <- glmFit(dat.y, design = mod1, dispersion = my_dispersions$tagwise.dispersion, offset = log(sequencing_depth$N))
   my_tests <- glmLRT(my_fits, coef = "zFER")
@@ -41,6 +48,7 @@ run_edgeR <- function(dat.x, dat.y) {
   return(my_stats)
 }
 
+# load gene expression data
 gene.annotation <- read_xlsx("1605_Paalvast_Lexogen.expression.genelevel.v82.htseq.xlsx", range = "A1:B22898")
 gene.annotation <- gene.annotation %>% 
   rename(gene = probe, 
@@ -48,12 +56,21 @@ gene.annotation <- gene.annotation %>%
 
 rna <- read_tsv("1605_Paalvast_Lexogen.expression.genelevel.v82.htseq.txt.table")
 
-sequencing_depth <- rna %>% 
-  gather(mouse, y, -probe, -`gene name`) %>% 
-  group_by(mouse) %>% 
-  summarise(N = sum(y)) %>% 
-  arrange(mouse)
+rna.batch1 <- read_xlsx("batch.xlsx")
+rna.batch2 <- read_tsv("keys.txt")
 
+rna.batch2 <- rna.batch2 %>% 
+  filter(!is.na(`Liver Weight`)) %>% 
+  rename(mouse = Samples) %>% 
+  select(mouse, Batch)
+
+rna.batch <- rna.batch1 %>% 
+  rename(R1 = `batch 1'`, 
+         R2 = `batch 2'`) %>% 
+  gather(Reagent, mouse) %>% 
+  left_join(rna.batch2, by = "mouse")
+
+# load mouse data
 paalvast_mice <- read_rds("Paalvast_lifespan.rds")
 paalvast_mice <- paalvast_mice %>% 
   mutate(day = interval(`Start HFD`, Sacrifice) / ddays(1), 
@@ -70,40 +87,20 @@ mouse.df <- tibble(mouse_id = as.character(seq(from = 1, to = 105)),
                    day = paalvast_mice$day, 
                    age_day = paalvast_mice$age_day)
 
-rna.batch1 <- read_xlsx("batch.xlsx")
-rna.batch2 <- read_tsv("keys.txt")
-
-rna.batch2 <- rna.batch2 %>% 
-  filter(!is.na(`Liver Weight`)) %>% 
-  rename(mouse = Samples) %>% 
-  select(mouse, Batch)
-
-rna.batch <- rna.batch1 %>% 
-  rename(R1 = `batch 1'`, 
-         R2 = `batch 2'`) %>% 
-  gather(Reagent, mouse) %>% 
-  left_join(rna.batch2, by = "mouse")
-
+# merge mouse data with gene expression data
 mouse_rna.df <- mouse.df %>% 
   semi_join(sequencing_depth, by = "mouse") %>% 
   left_join(rna.batch, by = "mouse") %>% 
   mutate(M_sacrifice = as.character(M_sacrifice), 
          Batch = as.character(Batch))
 
+#########################################################################
+# 2. Preprocessing
+#########################################################################
 
-# Preprocessing
-
-nonzero_counts <- rna %>% 
-  gather(mouse, y, -probe, -`gene name`) %>% 
-  group_by(probe) %>% 
-  summarize(n_nonzero = sum(y != 0))
-
-keep_genes <- nonzero_counts %>% 
-  filter(n_nonzero >= 43)
-
-# Feed efficiency
-
-mingled.mcmc <- read_stan_csv("~/cmdstan-2.19.1/mingled/output.csv")
+# load feed efficiency data
+#mingled.mcmc <- read_stan_csv("/home/xiang/stan/cmdstan-2.19.1/MINGLeD2/output.csv")
+mingled.mcmc <- read_stan_csv("/home/xiang/cmdstan-2.19.1/mingled/output.csv")
 
 tidy(mingled.mcmc, pars = c("fer_max", "bw_max"), estimate.method = "mean", conf.int = TRUE, conf.level = 0.95, conf.method = "HPDinterval")
 
@@ -119,7 +116,8 @@ fer <- post %>%
   inner_join(mouse_rna.df, by = "mouse_id") %>% 
   mutate(group = factor(group, levels = c("1M", "2M", "3M", "6MA", "6MC"), 
                         labels = c("1 month", "2 months", "3 months", "6 months", "6 months"))) %>% 
-  arrange(group)
+  arrange(group) %>% 
+  filter(group != "2 months") # 2 month feeding mice were processed only Reagent 2
 
 ggplot(fer, aes(fct_inorder(mouse), fer)) + 
   geom_boxplot(aes(fill = group)) + 
@@ -131,7 +129,25 @@ ggplot(fer, aes(fct_inorder(mouse), fer)) +
         axis.title.y = element_text(size = 15),
         axis.text.x = element_text(angle = 90))
 
-#ggsave("fe_sacrifice.pdf", height = 200, width = 220, units = "mm", dpi = 300)
+ggsave("fe_sacrifice.pdf", height = 200, width = 220, units = "mm", dpi = 300)
+
+# total counts for each RNA samples
+sequencing_depth <- rna %>% 
+  gather(mouse, y, -probe, -`gene name`) %>% 
+  filter(mouse %in% fer$mouse) %>% 
+  group_by(mouse) %>% 
+  summarise(N = sum(y)) %>% 
+  arrange(mouse)
+
+# genes with too many zero counts will be excluded
+nonzero_counts <- rna %>% 
+  gather(mouse, y, -probe, -`gene name`) %>% 
+  filter(mouse %in% fer$mouse) %>% 
+  group_by(probe) %>% 
+  summarize(n_nonzero = sum(y != 0))
+
+keep_genes <- nonzero_counts %>% 
+  filter(n_nonzero >= 30)
 
 dat.rna <- rna %>% 
   semi_join(keep_genes, by = "probe") %>% 
@@ -140,8 +156,12 @@ dat.rna <- rna %>%
   select(-gene) %>% 
   as.matrix()
 
+dat.rna <- dat.rna[,colnames(dat.rna) %in% fer$mouse]
 dat.rna <- dat.rna[,sort(unique(fer$mouse))]
 
+#################################################################
+# 3. Run negative binomial regression analysis
+#################################################################
 fer.nested <- fer %>% 
   arrange(mouse) %>% 
   group_by(.iteration) %>% 
@@ -169,8 +189,8 @@ fer.nested <- fer.nested %>%
   collect() %>% 
   as_tibble()
 
-#write_rds(fer.nested, "association_fer_transcriptomics.rds")
-fer.nested <- read_rds("association_fer_transcriptomics.rds")
+write_rds(fer.nested, "association_fer_transcriptomics.rds")
+#fer.nested <- read_rds("association_fer_transcriptomics.rds")
 
 fer2rna <- fer.nested %>% 
   select(-data) %>% 
@@ -181,20 +201,28 @@ fer2rna.sig <- fer2rna %>%
   summarise(`Mean beta` = mean(estimate), 
             `Low beta` = quantile(estimate, probs = 0.025), 
             `High beta` = quantile(estimate, probs = 0.975), 
-            `How many times FDR is below 0.05` = sum(FDR < 0.05)) %>% 
-  arrange(`Mean FDR`)
+            `Mean pval` = mean(p.value), 
+            `Low pval` = quantile(p.value, probs = 0.025), 
+            `High pval` = quantile(p.value, probs = 0.975), 
+            `Mean FDR` = mean(FDR), 
+            `Low FDR` = quantile(FDR, probs = 0.025), 
+            `High FDR` = quantile(FDR, probs = 0.975), 
+            `How many times FDR is below 0.05` = sum(FDR < 0.05))
 
-fer2rna.sig %>% 
-  filter(`How many times FDR is below 0.05` >= 990) %>% 
-  mutate(symbol = fct_inorder(symbol)) %>% 
-  ggplot() + 
-  geom_pointrange(aes(x = symbol, y = `Median beta`, ymin = `Low beta`, ymax = `High beta`), shape = 1) + 
-  coord_flip() + 
-  labs(x = NULL, y = "Median regression coeffcient with 95% credible interval") + 
+write_csv(fer2rna.sig, "Association_feed_efficiency_liver_gene_expression.csv")
+
+fer2rna.sig2 <- fer2rna.sig %>% 
+  filter(`How many times FDR is below 0.05` == 1000)
+
+# volcano plot
+ggplot(fer2rna.sig, aes(`Mean beta`, -log10(`Mean pval`))) + 
+  geom_point(alpha = 0.1) + 
+  geom_point(data = fer2rna.sig2, color = "red") + 
+  geom_text_repel(data = fer2rna.sig2, aes(label = symbol)) + 
+  labs(x = "Mean log(Fold change)", y = "-log10(Mean P value)") + 
   theme_bw() + 
-  theme(axis.text = element_text(size = 12), 
-        axis.title.x = element_text(size = 12))
+  theme(axis.title = element_text(size = 12))
 
-ggsave("asso_fer_liver_gene.pdf", height = 280, width = 220, units = "mm", dpi = 300)
+ggsave("asso_fer_liver_gene.pdf", height = 220, width = 220, units = "mm", dpi = 300)
 
 
