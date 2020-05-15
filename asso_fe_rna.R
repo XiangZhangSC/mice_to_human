caf9d4e3d418f6d98363f02b240af3a0ad6e1abf
Xiang Zhang <sc.xiang.zhang@gmail.com>
2020-05-15 14:45:28 +0200

    fix the order of commands

--- asso_fe_rna.R
+++ asso_fe_rna.R
@@ -12,7 +12,7 @@ library(multidplyr)
 library(broom)
 library(stringr)
 library(forcats)
-#library(ggrepel)
+library(ggrepel)
 
 ###########################################
 # 1. Import data
@@ -85,7 +85,30 @@ mouse.df <- tibble(mouse_id = as.character(seq(from = 1, to = 105)),
                    Y_sacrifice = as.character(paalvast_mice$Y_sacrifice), 
                    M_sacrifice = as.character(paalvast_mice$M_sacrifice), 
                    day = paalvast_mice$day, 
-                   age_day = paalvast_mice$age_day)
+                   age_day = paalvast_mice$age_day) %>% 
+  filter(group != "2M") # 2 month feeding mice were processed only Reagent 2
+
+#########################################################################
+# 2. Preprocessing
+#########################################################################
+
+# load feed efficiency data
+mingled.mcmc <- read_stan_csv("/home/xiang/stan/cmdstan-2.19.1/MINGLeD2/output.csv")
+#mingled.mcmc <- read_stan_csv("/home/xiang/cmdstan-2.19.1/mingled/output.csv")
+
+tidy(mingled.mcmc, pars = c("fer_max", "bw_max"), estimate.method = "mean", conf.int = TRUE, conf.level = 0.95, conf.method = "HPDinterval")
+
+tidy(mingled.mcmc, pars = c("mu_fi", "sigma_fi"), estimate.method = "mean", conf.int = TRUE, conf.level = 0.95, conf.method = "HPDinterval")
+
+post <- tidy_draws(mingled.mcmc)
+
+# total counts for each RNA samples
+sequencing_depth <- rna %>% 
+  gather(mouse, y, -probe, -`gene name`) %>% 
+  filter(mouse %in% mouse.df$mouse) %>% 
+  group_by(mouse) %>% 
+  summarise(N = sum(y)) %>% 
+  arrange(mouse)
 
 # merge mouse data with gene expression data
 mouse_rna.df <- mouse.df %>% 
@@ -93,20 +116,6 @@ mouse_rna.df <- mouse.df %>%
   left_join(rna.batch, by = "mouse") %>% 
   mutate(M_sacrifice = as.character(M_sacrifice), 
          Batch = as.character(Batch))
-
-#########################################################################
-# 2. Preprocessing
-#########################################################################
-
-# load feed efficiency data
-#mingled.mcmc <- read_stan_csv("/home/xiang/stan/cmdstan-2.19.1/MINGLeD2/output.csv")
-mingled.mcmc <- read_stan_csv("/home/xiang/cmdstan-2.19.1/mingled/output.csv")
-
-tidy(mingled.mcmc, pars = c("fer_max", "bw_max"), estimate.method = "mean", conf.int = TRUE, conf.level = 0.95, conf.method = "HPDinterval")
-
-tidy(mingled.mcmc, pars = c("mu_fi", "sigma_fi"), estimate.method = "mean", conf.int = TRUE, conf.level = 0.95, conf.method = "HPDinterval")
-
-post <- tidy_draws(mingled.mcmc)
 
 fer <- post %>% 
   select(.iteration, fer_sacrifice.1:fer_sacrifice.105) %>% 
@@ -116,8 +125,7 @@ fer <- post %>%
   inner_join(mouse_rna.df, by = "mouse_id") %>% 
   mutate(group = factor(group, levels = c("1M", "2M", "3M", "6MA", "6MC"), 
                         labels = c("1 month", "2 months", "3 months", "6 months", "6 months"))) %>% 
-  arrange(group) %>% 
-  filter(group != "2 months") # 2 month feeding mice were processed only Reagent 2
+  arrange(group)
 
 ggplot(fer, aes(fct_inorder(mouse), fer)) + 
   geom_boxplot(aes(fill = group)) + 
@@ -130,14 +138,6 @@ ggplot(fer, aes(fct_inorder(mouse), fer)) +
         axis.text.x = element_text(angle = 90))
 
 ggsave("fe_sacrifice.pdf", height = 200, width = 220, units = "mm", dpi = 300)
-
-# total counts for each RNA samples
-sequencing_depth <- rna %>% 
-  gather(mouse, y, -probe, -`gene name`) %>% 
-  filter(mouse %in% fer$mouse) %>% 
-  group_by(mouse) %>% 
-  summarise(N = sum(y)) %>% 
-  arrange(mouse)
 
 # genes with too many zero counts will be excluded
 nonzero_counts <- rna %>% 
@@ -211,18 +211,20 @@ fer2rna.sig <- fer2rna %>%
 
 write_csv(fer2rna.sig, "Association_feed_efficiency_liver_gene_expression.csv")
 
-#fer2rna.sig2 <- fer2rna.sig %>% 
-#  filter(`How many times FDR is below 0.05` == 1000)
+fer2rna.sig2 <- fer2rna.sig %>% 
+  filter(`How many times FDR is below 0.05` == 1000)
 
 # volcano plot
-#ggplot(fer2rna.sig, aes(`Mean beta`, -log10(`Mean pval`))) + 
-#  geom_point(alpha = 0.1) + 
-#  geom_point(data = fer2rna.sig2, color = "red") + 
-#  geom_text_repel(data = fer2rna.sig2, aes(label = symbol)) + 
-#  labs(x = "Mean log(Fold change)", y = "-log10(Mean P value)") + 
-#  theme_bw() + 
-#  theme(axis.title = element_text(size = 12))
+ggplot(fer2rna.sig, aes(`Mean beta`, -log10(`Mean pval`))) + 
+  geom_point(alpha = 0.1) + 
+  geom_point(data = fer2rna.sig2, color = "red") + 
+  geom_text_repel(data = fer2rna.sig2, aes(label = symbol)) + 
+  labs(x = "Mean log(Fold change)", y = "-log10(Mean P value)") + 
+  facet_warp(~group, scale = "free_y", ncol = 3)
+  theme_bw() + 
+  theme(axis.title = element_text(size = 12), 
+        strip.text = element_text(size = 12))
 
-#ggsave("asso_fer_liver_gene.pdf", height = 220, width = 220, units = "mm", dpi = 300)
+ggsave("asso_fer_liver_gene.pdf", height = 100, width = 300, units = "mm", dpi = 300)
 
 
