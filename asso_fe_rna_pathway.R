202cbc30bea71f78cba96692bddc60092716007d
Xiang Zhang <sc.xiang.zhang@gmail.com>
2020-06-02 11:16:33 +0200

    fix the bugs in pathway analysis

--- asso_fe_rna_pathway.R
+++ asso_fe_rna_pathway.R
@@ -6,9 +6,14 @@ source("data_for_asso_fe_rna.R")
 # negative binomial regression model
 calc_path_stat <- function(dat.y, mod, sequencing_depth) {
   my_dispersions <- estimateDisp(dat.y, design = mod)
-  my_fits <- glmFit(dat.y, design = mod, dispersion = my_dispersions$tagwise.dispersion, offset = log(sequencing_depth$N))
+  my_fits <- glmFit(dat.y, 
+                    design = mod, 
+                    dispersion = my_dispersions$tagwise.dispersion, 
+                    offset = log(sequencing_depth$N))
   my_tests <- glmLRT(my_fits, coef = "zFER")
-  my_gsea <- kegga(my_tests, species = 'Mm')
+  my_gsea <- kegga(my_tests, 
+                   gene.pathway = mouse_kegg_pathway, 
+                   pathway.names = mouse_kegg_pathway_name)
   
   return(my_gsea)
 }
@@ -73,6 +78,9 @@ run_path_3 <- function(dat.x, dat.y, sequencing_depth) {
 # 3.2 Run negative binomial regression
 #
 
+mouse_kegg_pathway <- getGeneKEGGLinks(species.KEGG = 'mmu', convert = TRUE)
+mouse_kegg_pathway_name <- getKEGGPathwayNames(species.KEGG = 'mmu')
+
 # 1M group
 xiang_cluster_1 <- new_cluster(n = 22) %>% 
   cluster_library("edgeR") %>% 
@@ -83,14 +91,18 @@ xiang_cluster_1 <- new_cluster(n = 22) %>%
   cluster_copy("build_mod_1") %>% 
   cluster_copy("run_path_1") %>% 
   cluster_copy("dat.rna.1M") %>% 
-  cluster_copy("ensembl_ncbi") %>% 
   cluster_copy("sequencing_depth_1M") %>% 
-  cluster_copy("calc_path_stat")
+  cluster_copy("calc_path_stat") %>% 
+  cluster_copy("mouse_kegg_pathway") %>% 
+  cluster_copy("mouse_kegg_pathway_name")
 
 fer_1M_nested <- fer_1M_nested %>% 
   group_by(.iteration) %>% 
   partition(cluster = xiang_cluster_1) %>% 
-  mutate(result = map(data, ~run_path_1(dat.x = .x, dat.y = dat.rna.1M, sequencing_depth = sequencing_depth_1M))) %>% 
+  mutate(result = map(data, 
+                      ~run_path_1(dat.x = .x, 
+                                  dat.y = dat.rna.1M, 
+                                  sequencing_depth = sequencing_depth_1M))) %>% 
   collect() %>% 
   as_tibble()
 
@@ -108,14 +120,18 @@ xiang_cluster_2 <- new_cluster(n = 22) %>%
   cluster_copy("build_mod_2") %>% 
   cluster_copy("run_path_2") %>% 
   cluster_copy("dat.rna.2M") %>% 
-  cluster_copy("ensembl_ncbi") %>% 
   cluster_copy("sequencing_depth_2M") %>% 
-  cluster_copy("calc_path_stat") 
+  cluster_copy("calc_path_stat") %>% 
+  cluster_copy("mouse_kegg_pathway") %>% 
+  cluster_copy("mouse_kegg_pathway_name")
 
 fer_2M_nested <- fer_2M_nested %>% 
   group_by(.iteration) %>% 
   partition(cluster = xiang_cluster_2) %>% 
-  mutate(result = map(data, ~run_path_2(dat.x = .x, dat.y = dat.rna.2M, sequencing_depth = sequencing_depth_2M))) %>% 
+  mutate(result = map(data, 
+                      ~run_path_2(dat.x = .x, 
+                                  dat.y = dat.rna.2M, 
+                                  sequencing_depth = sequencing_depth_2M))) %>% 
   collect() %>% 
   as_tibble()
 
@@ -133,14 +149,18 @@ xiang_cluster_3 <- new_cluster(n = 22) %>%
   cluster_copy("build_mod_1") %>% 
   cluster_copy("run_path_1") %>% 
   cluster_copy("dat.rna.3M") %>% 
-  cluster_copy("ensembl_ncbi") %>% 
   cluster_copy("sequencing_depth_3M") %>% 
-  cluster_copy("calc_path_stat")
+  cluster_copy("calc_path_stat") %>% 
+  cluster_copy("mouse_kegg_pathway") %>% 
+  cluster_copy("mouse_kegg_pathway_name")
 
 fer_3M_nested <- fer_3M_nested %>% 
   group_by(.iteration) %>% 
   partition(cluster = xiang_cluster_3) %>% 
-  mutate(result = map(data, ~run_path_1(dat.x = .x, dat.y = dat.rna.3M, sequencing_depth = sequencing_depth_3M))) %>% 
+  mutate(result = map(data, 
+                      ~run_path_1(dat.x = .x, 
+                                  dat.y = dat.rna.3M, 
+                                  sequencing_depth = sequencing_depth_3M))) %>% 
   collect() %>% 
   as_tibble()
 
@@ -158,14 +178,18 @@ xiang_cluster_4 <- new_cluster(n = 22) %>%
   cluster_copy("build_mod_3") %>% 
   cluster_copy("run_path_3") %>% 
   cluster_copy("dat.rna.6M") %>% 
-  cluster_copy("ensembl_ncbi") %>% 
   cluster_copy("sequencing_depth_6M") %>% 
-  cluster_copy("calc_path_stat")
+  cluster_copy("calc_path_stat") %>% 
+  cluster_copy("mouse_kegg_pathway") %>% 
+  cluster_copy("mouse_kegg_pathway_name")
 
 fer_6M_nested <- fer_6M_nested %>% 
   group_by(.iteration) %>% 
   partition(cluster = xiang_cluster_4) %>% 
-  mutate(result = map(data, ~run_path_3(dat.x = .x, dat.y = dat.rna.6M, sequencing_depth = sequencing_depth_6M))) %>% 
+  mutate(result = map(data, 
+                      ~run_path_3(dat.x = .x, 
+                                  dat.y = dat.rna.6M, 
+                                  sequencing_depth = sequencing_depth_6M))) %>% 
   collect() %>% 
   as_tibble()
 
